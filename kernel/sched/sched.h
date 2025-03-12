f56bdd8423e2561c4b9dea5834ca01c7edc865c1
Dhaval Giani (AMD) <dhaval@gianis.ca>
2025-03-12 10:27:31 -0700

    sched/fair: Introduce a new debugfs directory for EEVDF tests
    Signed-off-by: Dhaval Giani (AMD) <dhaval@gianis.ca>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -44,6 +44,7 @@
 #include <linux/lockdep.h>
 #include <linux/minmax.h>
 #include <linux/mm.h>
+#include <linux/mmu_context.h>
 #include <linux/module.h>
 #include <linux/mutex_api.h>
 #include <linux/plist.h>
@@ -4002,5 +4003,11 @@ void sched_enq_and_set_task(struct sched_enq_and_set_ctx *ctx);
 #endif /* CONFIG_SCHED_CLASS_EXT */
 
 #include "ext.h"
+
+#ifdef CONFIG_SCHED_EEVDF_TESTING
+void debugfs_eevdf_testing_init(struct dentry *debugfs_sched);
+#else /* CONFIG_SCHED_EEVDF_TESTING */
+static inline void debugfs_eevdf_testing_init(struct dentry *debugfs_sched) {}
+#endif /* CONFIG_SCHED_EEVDF_TESTING */
 
 #endif /* _KERNEL_SCHED_SCHED_H */
