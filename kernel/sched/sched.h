a890ad3229f9a6fe5b72124a431125d348565c58
Dhaval Giani (AMD) <dhaval@gianis.ca>
2025-03-12 10:53:56 -0700

    sched/fair: Add a test to test that a task selected to run has positive lag
    Lemma 1 from the original EEVDF paper says that any task that
    has a positive lag is eligible to run.
    
    This test tests the opposite - any task that is picked to run must
    have a positive lag for it to be eligible to run.
    
    Signed-off-by: Dhaval Giani (AMD) <dhaval@gianis.ca>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3988,8 +3988,10 @@ DEFINE_CLASS_IS_UNCONDITIONAL(sched_change)
 
 #ifdef CONFIG_SCHED_EEVDF_TESTING
 void debugfs_eevdf_testing_init(struct dentry *debugfs_sched);
+void test_eevdf_positive_lag(struct cfs_rq *cfs, struct sched_entity *se);
 #else /* CONFIG_SCHED_EEVDF_TESTING */
 static inline void init_eevdf_testing_debugfs(struct dentry *debugfs_sched) {}
+static inline void test_eevdf_positive_lag(struct cfs_rq *cfs, struct sched_entity *se) {}
 #endif /* CONFIG_SCHED_EEVDF_TESTING */
 
 #endif /* _KERNEL_SCHED_SCHED_H */
