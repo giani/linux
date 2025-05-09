72c80ba7970384d9f02bc91a80e81b68cef27b3c
Dhaval Giani (AMD) <dhaval@gianis.ca>
2025-05-09 12:13:11 -0700

    sched/eevdf-tests: Add a test for Theorem 1 from the EEVDF Paper
    Theorem 1 in the paper establishes strict bounds for maximum
    and minimum lag as equal to the zero vruntime + deadline, or
    zero_vruntime - deadline.
    
    The Linux implemention exploits this property and cheats when
    the lag exceeeds these limits and clamps them to the limit.
    
    Test how often these limits are actually violated.
    
    Signed-off-by: Dhaval Giani (AMD) <dhaval@gianis.ca>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -287,7 +287,7 @@ static u64 __calc_delta(u64 delta_exec, unsigned long weight, struct load_weight
 /*
  * delta /= w
  */
-static inline u64 calc_delta_fair(u64 delta, struct sched_entity *se)
+inline u64 calc_delta_fair(u64 delta, struct sched_entity *se)
 {
 	if (unlikely(se->load.weight != NICE_0_LOAD))
 		delta = __calc_delta(delta, NICE_0_LOAD, &se->load);
